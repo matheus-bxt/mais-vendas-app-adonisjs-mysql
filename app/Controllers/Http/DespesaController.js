3a5dd4082e576ef1e8e375b63a3887ef4be728fb
matheus-bxt <matheus.bxt@gmail.com>
2020-06-18 21:11:03 -0300

    Ajustes DespesaController

--- app/Controllers/Http/DespesaController.js
+++ app/Controllers/Http/DespesaController.js
@@ -6,56 +6,15 @@ const Despesa = use('App/Models/Despesa');
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with despesas
- */
 class DespesaController {
-  /**
-   * Show a list of all despesas.
-   * GET despesas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
 
-  /**
-   * Render a form to be used for creating a new despesa.
-   * GET despesas/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new despesa.
-   * POST despesas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-    // Extrai os dados do request
-    const data = request.only(['filial_id', 'descricao', 'data', 'valor']);
-
-    // Cria nova despesa com os dados do request
-    await Despesa.create(data);    
-    return response.redirect('/despesas');
-  }
-
-  async despesasView({ view, auth }) {
+  //MÉTODOS GET
+  async despesasView({ auth, view }) {
     const despesas = await Despesa
     .query()
     .where('filial_id', auth.user.filial_id)
     .orderBy('data', 'desc')
+    .orderBy('id', 'desc')
     .fetch();
 
     return view.render('pages.despesas.despesas', { despesas: despesas.toJSON() });
@@ -65,87 +24,15 @@ class DespesaController {
     return view.render('pages.despesas.cadastrarDespesa');
   }
 
-  async alterarDespesaView({ view, params, session, response }) {
+  async alterarDespesaView({ params, session, response, view }) {
     var despesa = await Despesa.findBy('id', params.despesa_id);
     if (despesa == null) {
-      //retorna mensagem de erro para informar não foi encontrada a despesa com o id informado
-      session.flash({openToEditDespesaError: 'Não foi encontrada a despesa com o id informado!'})
-      return response.redirect('back');
-    }
-
-    return view.render('pages.despesas.alterarDespesa', { despesa: despesa.toJSON() });
-  }
-
-  /**
-   * Display a single despesa.
-   * GET despesas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing despesa.
-   * GET despesas/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update despesa details.
-   * PUT or PATCH despesas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response, session }) {
-    // Busca a despesa cadastrada com o parametro despesa_id
-    const despesa = await Despesa.find(params.despesa_id);
-    if (despesa == null) {
       //retorna mensagem de erro para informar que a despesa não existe mais
-      session.flash({updateDespesaError: 'A despesa não existe mais!'})
+      session.flash({openToEditDespesaError: 'A despesa não existe mais!'})
       return response.redirect('/despesas');
     }
 
-    // Extrai os dados do request
-    const data = request.only(['descricao', 'data', 'valor']);
-    
-    // Atualiza e salva a despesa
-    despesa.merge(data);
-    await despesa.save();
-    
-    return response.redirect('/despesas');
-  }
-
-  /**
-   * Delete a despesa with id.
-   * DELETE despesas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response, session }) {
-    var despesa = await Despesa.findBy('id', params.despesa_id);
-    if (despesa == null) {
-      //retorna mensagem de erro para informar não foi encontrada a despesa com o id informado
-      session.flash({deleteDespesaError: 'Não foi encontrada a despesa com o id informado!'});
-      return response.redirect('back');
-    }
-
-    await despesa.delete();
-
-    return response.redirect('/despesas');
+    return view.render('pages.despesas.alterarDespesa', { despesa: despesa.toJSON() });
   }
 
   async relatorioDespesasView ({ request, auth, view }) {
@@ -156,6 +43,8 @@ class DespesaController {
     .where('filial_id', auth.user.filial_id)
     .andWhere('data', '>=', data_inicial)
     .andWhere('data', '<=', data_final)
+    .orderBy('data', 'asc')
+    .orderBy('id', 'asc')
     .fetch();
 
     const valorTotalDespesas = despesas.toJSON().reduce((a, { valor }) => a + (valor), 0);
@@ -165,6 +54,46 @@ class DespesaController {
     return view.render('pages.despesas.relatorioDespesas', { data_inicial: dt_inicial, data_final: dt_final, despesas: despesas.toJSON(), valorTotalDespesas: valorTotalDespesas })
   }
 
+  //MÉTODOS POST
+  async store ({ request, response }) {
+    const dados = request.only(['filial_id', 'descricao', 'data', 'valor']);
+
+    await Despesa.create(dados);    
+    return response.redirect('/despesas');
+  }
+
+  //MÉTODOS PUT  
+  async update ({ params, request, response, session }) {
+    const despesa = await Despesa.find(params.despesa_id);
+    if (despesa == null) {
+      //retorna mensagem de erro para informar que a despesa não existe mais
+      session.flash({updateDespesaError: 'A despesa não existe mais!'})
+      return response.redirect('/despesas');
+    }
+
+    const dados = request.only(['descricao', 'data', 'valor']);
+
+    despesa.merge(dados);
+    await despesa.save();
+    
+    return response.redirect('/despesas');
+  }
+  
+  //MÉTODOS DELETE
+  async destroy ({ params, request, response, session }) {
+    var despesa = await Despesa.findBy('id', params.despesa_id);
+    if (despesa == null) {
+      //retorna mensagem de erro para informar que a despesa não existe mais
+      session.flash({deleteDespesaError: 'A despesa não existe mais!'});
+      return response.redirect('/despesas');
+    }
+
+    await despesa.delete();
+
+    return response.redirect('/despesas');
+  }
+
+  //OUTROS MÉTODOS
   async formataData(data) {
     var split = data.split('-');
     return split[2] + '/' + split[1] + '/' + split[0];
--- start/routes.js
+++ start/routes.js
@@ -55,38 +55,38 @@ Route.group('userauth', () => {
         Route.get('/relatorioPedidos', 'PedidoController.relatorioPedidosView').as('view.relatorio-pedidos').middleware(['admin']);;
 
     //CREATE
-        Route.post('/cadastrarFilial', 'FilialController.store').as('store.filial').prefix('api').validator('CreateFilial').middleware(['admin']);
-        Route.post('/cadastrarUsuario', 'UserController.store').as('store.usuario').prefix('api').validator('CreateUser').middleware(['admin']);
-        Route.post('/cadastrarDespesa', 'DespesaController.store').as('store.despesa').prefix('api').validator('CreateDespesa').middleware(['admin']);
-        Route.post('/cadastrarProduto', 'ProdutoController.store').as('store.produto').prefix('api').validator('CreateProduto').middleware(['admin']);
-        Route.post('/cadastrarMesa', 'MesaController.store').as('store.mesa').prefix('api').validator('CreateMesa');
-        Route.post('/cadastrarPedido', 'PedidoController.store').as('store.pedido').prefix('api').validator('CreatePedido');
-        Route.post('/adicionarProdutoPedido', 'PedidoController.storeProdutoPedido').as('store.produto-pedido').prefix('api')
+    Route.post('/cadastrarFilial', 'FilialController.store').as('store.filial').prefix('api').validator('CreateFilial').middleware(['admin']);
+    Route.post('/cadastrarUsuario', 'UserController.store').as('store.usuario').prefix('api').validator('CreateUser').middleware(['admin']);
+    Route.post('/cadastrarDespesa', 'DespesaController.store').as('store.despesa').prefix('api').validator('CreateDespesa').middleware(['admin']);
+    Route.post('/cadastrarProduto', 'ProdutoController.store').as('store.produto').prefix('api').validator('CreateProduto').middleware(['admin']);
+    Route.post('/cadastrarMesa', 'MesaController.store').as('store.mesa').prefix('api').validator('CreateMesa');
+    Route.post('/cadastrarPedido', 'PedidoController.store').as('store.pedido').prefix('api').validator('CreatePedido');
+    Route.post('/adicionarProdutoPedido', 'PedidoController.storeProdutoPedido').as('store.produto-pedido').prefix('api')
 
     //UPDATE
-        Route.put('/alterarFilial/:filial_id', 'FilialController.update').as('update.filial').prefix('api').validator('CreateFilial').middleware(['admin']);
-        Route.put('/alterarUsuario/:usuario_id', 'UserController.update').as('update.usuario').prefix('api').validator('CreateUser').middleware(['admin']);
-        Route.put('/alterarDespesa/:despesa_id', 'DespesaController.update').as('update.despesa').prefix('api').validator('CreateDespesa').middleware(['admin']);
-        Route.put('/alterarProduto/:produto_id', 'ProdutoController.update').as('update.produto').prefix('api').validator('CreateProduto').middleware(['admin']);
-        Route.put('/alterarMesa/:mesa_id', 'MesaController.update').as('update.mesa').prefix('api').validator('CreateMesa').middleware(['admin']);
-        Route.put('/alterarPedido/:pedido_id', 'PedidoController.update').as('update.pedido').prefix('api').validator('CreatePedido');
-        Route.put('/alterarProdutoPedido/:id', 'PedidoController.updateProdutoPedido').as('update.produto-pedido').prefix('api');
-        Route.put('/alterarStatusPedido/:pedido_id', 'PedidoController.updateStatusPedido').as('update.status-pedido').prefix('api');
+    Route.put('/alterarFilial/:filial_id', 'FilialController.update').as('update.filial').prefix('api').validator('CreateFilial').middleware(['admin']);
+    Route.put('/alterarUsuario/:usuario_id', 'UserController.update').as('update.usuario').prefix('api').validator('CreateUser').middleware(['admin']);
+    Route.put('/alterarDespesa/:despesa_id', 'DespesaController.update').as('update.despesa').prefix('api').validator('CreateDespesa').middleware(['admin']);
+    Route.put('/alterarProduto/:produto_id', 'ProdutoController.update').as('update.produto').prefix('api').validator('CreateProduto').middleware(['admin']);
+    Route.put('/alterarMesa/:mesa_id', 'MesaController.update').as('update.mesa').prefix('api').validator('CreateMesa').middleware(['admin']);
+    Route.put('/alterarPedido/:pedido_id', 'PedidoController.update').as('update.pedido').prefix('api').validator('CreatePedido');
+    Route.put('/alterarProdutoPedido/:id', 'PedidoController.updateProdutoPedido').as('update.produto-pedido').prefix('api');
+    Route.put('/alterarStatusPedido/:pedido_id', 'PedidoController.updateStatusPedido').as('update.status-pedido').prefix('api');
 
     //DELETE
-        Route.delete('/excluirFilial/:filial_id', 'FilialController.destroy').as('destroy.filial').prefix('api').middleware(['admin']);
-        Route.delete('/excluirUsuario/:usuario_id', 'UserController.destroy').as('destroy.usuario').prefix('api').middleware(['admin']);
-        Route.delete('/excluirDespesa/:despesa_id', 'DespesaController.destroy').as('destroy.despesa').prefix('api').middleware(['admin']);
-        Route.delete('/excluirProduto/:produto_id', 'ProdutoController.destroy').as('destroy.produto').prefix('api').middleware(['admin']);
-        Route.delete('/excluirMesa/:mesa_id', 'MesaController.destroy').as('destroy.mesa').prefix('api').middleware(['admin']);
-        Route.delete('/excluirPedido/:pedido_id', 'PedidoController.destroy').as('destroy.pedido').prefix('api').middleware(['admin']);
-        Route.delete('/excluirProdutoPedido/:id', 'PedidoController.destroyProdutoPedido').as('destroy.produto-pedido').prefix('api')
+    Route.delete('/excluirFilial/:filial_id', 'FilialController.destroy').as('destroy.filial').prefix('api').middleware(['admin']);
+    Route.delete('/excluirUsuario/:usuario_id', 'UserController.destroy').as('destroy.usuario').prefix('api').middleware(['admin']);
+    Route.delete('/excluirDespesa/:despesa_id', 'DespesaController.destroy').as('destroy.despesa').prefix('api').middleware(['admin']);
+    Route.delete('/excluirProduto/:produto_id', 'ProdutoController.destroy').as('destroy.produto').prefix('api').middleware(['admin']);
+    Route.delete('/excluirMesa/:mesa_id', 'MesaController.destroy').as('destroy.mesa').prefix('api').middleware(['admin']);
+    Route.delete('/excluirPedido/:pedido_id', 'PedidoController.destroy').as('destroy.pedido').prefix('api').middleware(['admin']);
+    Route.delete('/excluirProdutoPedido/:id', 'PedidoController.destroyProdutoPedido').as('destroy.produto-pedido').prefix('api')
 
     //LOGOUT
-        Route.get('/logout', async ({ auth, response }) => {
-            await auth.logout();
-            return response.redirect('/');
-        }).prefix('api');
+    Route.get('/logout', async ({ auth, response }) => {
+        await auth.logout();
+        return response.redirect('/');
+    }).prefix('api');
 }).middleware(['userauth'])
 
 Route.group(() => {
